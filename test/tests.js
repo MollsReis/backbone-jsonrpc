d9d7ca2c5f1d0bb0349ec4032a5141499aadc0c4
Scott Reis <scott.reis@vertive.com>
2013-03-12 16:04:36 -0500

    write a test to check for named parameter functionality

--- test/tests.js
+++ test/tests.js
@@ -34,7 +34,7 @@ asyncTest('make good request (no params)', 2, function() {
     });
 });
 
-asyncTest('make good request (with params)', 3, function() {
+asyncTest('make good request (with unnamed params)', 3, function() {
     var Model = Backbone.Model.extend({ url: this.fakeUrl, method: 'sum', params: [5,6] })
     , myModel = new Model();
 
@@ -49,7 +49,29 @@ asyncTest('make good request (with params)', 3, function() {
         });
 
         equal(data.method, 'sum');
-        deepEqual(data.params, ["5", "6"]);
+        deepEqual(data.params, ['5', '6']);
+
+        req.respond(200, { "Content-Type": "text/json", "Content-Length": responseBody.length }, responseBody);
+    });
+});
+
+asyncTest('make good request (with named params)', 4, function() {
+    var Model = Backbone.Model.extend({ url: this.fakeUrl, method: 'diff', params: { second: 5, first: 10 } })
+        , myModel = new Model();
+
+    myModel.fetch({ success: function() { equal(myModel.get('mydiff'), 5); start(); } });
+
+    this.server.respondWith('post', this.fakeUrl, function(req) {
+        var data = qs.parse(req.requestBody)
+            , responseBody = JSON.stringify({
+                jsonrpc : '2.0',
+                id      : 1,
+                result  : { mydiff: 5 }
+            });
+
+        equal(data.method, 'diff');
+        equal(data.params.first, '10');
+        equal(data.params.second, '5');
 
         req.respond(200, { "Content-Type": "text/json", "Content-Length": responseBody.length }, responseBody);
     });
